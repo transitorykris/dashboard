e6cdb52980e43ac269e0a1ee39b2be4e961a814d
Kris Foster <kris.foster@gmail.com>
2022-12-24 17:34:48 -0800

    Use egui/eframe for the UI

--- src/dashboard.rs
+++ src/dashboard.rs
@@ -1,106 +1,76 @@
-use gtk::prelude::*;
-use relm4::prelude::*;
-use std::io::{self, Write};
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::{egui, CreationContext};
 use std::path::Path;
+use std::sync::Arc;
+use std::sync::Mutex;
+use tokio::runtime;
 use tokio::sync::mpsc;
 
 use logger::Logger;
 use rbmini::connection::RbManager;
-use rbmini::message::{decode_rb_message, rb_checksum, RbMessage};
+use rbmini::message::{decode_rb_message, RbMessage};
+
+use super::http;
 
 const LOG_FILE: &str = "/tmp/openlaps_dashboard_testing.db";
 
 struct DashboardApp {
-    telemetry: RbMessage,
-    status: String,
+    _rt: runtime::Runtime,
+    telemetry: Arc<Mutex<RbMessage>>,
+    status: Arc<Mutex<String>>,
 }
 
-#[derive(Debug)]
-enum Msg {
-    Update(RbMessage),
-    Status(String),
-}
+impl DashboardApp {
+    fn new(ctx: &CreationContext) -> Self {
+        let _rt = runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap();
 
-#[relm4::component]
-impl SimpleComponent for DashboardApp {
-    type Init = RbMessage;
-    type Input = Msg;
-    type Output = ();
-
-    view! {
-        gtk::Window {
-            set_title: Some("Openlaps Dashboard"),
-            set_default_size: (800, 600),
-            set_fullscreened: true,
-
-            gtk::Box {
-                set_orientation: gtk::Orientation::Vertical,
-                set_spacing: 5,
-                set_margin_all: 5,
-
-                gtk::Label {
-                    #[watch]
-                    set_label: &format!("GPS Coordinates: {}", model.telemetry.gps_coordinates()),
-                    set_margin_all: 5,
-                },
-                gtk::Label {
-                    #[watch]
-                    set_label: &format!("Speed: {:.1}", model.telemetry.speed()),
-                    set_margin_all: 5,
-                },
-                gtk::Label {
-                    #[watch]
-                    set_label: &format!("GPS Fix: {}", model.telemetry.is_valid_fix()),
-                    set_margin_all: 5,
-                },
-                gtk::Label {
-                    #[watch]
-                    set_label: &model.status,
-                    set_margin_all: 5,
-                }
-            }
-        }
-    }
-
-    // Initialize the component.
-    fn init(
-        telemetry: Self::Init,
-        root: &Self::Root,
-        sender: ComponentSender<Self>,
-    ) -> ComponentParts<Self> {
-        let model = DashboardApp {
-            telemetry,
-            status: String::new(),
-        };
-
-        // Insert the code generation of the view! macro here
-        let widgets = view_output!();
-
-        // Spawn the core logic of the dashboard
-        tokio::spawn(async move {
-            updater(sender).await;
+        // Start the telemetry updater
+        let telemetry = Arc::new(Mutex::new(RbMessage::new()));
+        let telemetry_clone = telemetry.clone();
+        let status = Arc::new(Mutex::new(String::from("Initializing")));
+        let status_clone = status.clone();
+        let ctx_copy = ctx.egui_ctx.clone();
+        _rt.spawn(async move {
+            updater(ctx_copy, telemetry_clone, status_clone).await;
         });
 
-        ComponentParts { model, widgets }
-    }
-
-    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
-        match msg {
-            Msg::Update(t) => {
-                self.telemetry = t;
-            }
-            Msg::Status(s) => {
-                self.status = s;
-            }
+        // Start the HTTP server
+        _rt.spawn(async move {
+            http::start().await;
+        });
+        Self {
+            _rt,
+            telemetry,
+            status,
         }
+    }
+}
+
+impl eframe::App for DashboardApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let t = self.telemetry.lock().unwrap();
+        let status = self.status.lock().unwrap();
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Openlaps Dashboard");
+            ui.label(format!("Status: {}", status));
+            ui.label(format!("GPS Coordinates: {}", t.gps_coordinates()));
+            ui.label(format!("GPS Fix: {}", t.is_valid_fix()));
+        });
     }
 }
 
 // For lack of a better name, this is the core logic
-async fn updater(sender: ComponentSender<DashboardApp>) {
-    // Connect to a racebox mini
-    // XXX This is all very explode-y right now
-    sender.input(Msg::Status("Setting up RaceBox mini manager".to_string()));
+async fn updater(
+    ctx: eframe::egui::Context,
+    telemetry: Arc<Mutex<RbMessage>>,
+    status: Arc<Mutex<String>>,
+) {
+    *status.lock().unwrap() = String::from("Creating RB Manager");
+    ctx.request_repaint();
     let mut rb = match RbManager::new().await {
         Err(e) => {
             panic!("{}", e);
@@ -108,53 +78,64 @@ async fn updater(sender: ComponentSender<DashboardApp>) {
         Ok(rb) => rb,
     };
 
-    sender.input(Msg::Status("Connecting to RaceBox mini".to_string()));
+    *status.lock().unwrap() = String::from("Connecting to RB");
+    ctx.request_repaint();
+    let status_clone = status.clone();
+    let ctx_clone = ctx.clone();
     let rc = match rb.connect().await {
         Err(e) => {
-            sender.input(Msg::Status("Failed to connect to RaceBox mini".to_string()));
-            panic!("{}", e);
+            *status_clone.lock().unwrap() = e;
+            ctx_clone.request_repaint();
+            panic!("Failed to connect to RB");
         }
         Ok(conn) => conn,
     };
-    sender.input(Msg::Status("".to_string()));
 
     // Create a logger to record telemetry to
     let logger = Logger::new(Path::new(LOG_FILE));
 
     let (tx, mut rx) = mpsc::channel(32);
 
+    *status.lock().unwrap() = String::from("Starting RB stream");
+    ctx.request_repaint();
     // Start another thread to stream from the racebox mini
+    let status_clone = status.clone();
+    let ctx_clone = ctx.clone();
     tokio::spawn(async move {
         if let Err(err) = rc.stream(tx).await {
+            *status_clone.lock().unwrap() = format!("{}", err);
+            ctx_clone.request_repaint();
             panic!("{}", err)
         }
     });
 
+    *status.lock().unwrap() = String::from("Running");
+    ctx.request_repaint();
     // Our receive loop, get a message from the racebox, send it to our app
-    let mut checksum_failures = 0;
     while let Some(msg) = rx.recv().await {
-        if !rb_checksum(&msg.value) {
-            checksum_failures += 1;
-        }
         let rb_msg = decode_rb_message(&msg.value);
-        // Just here to aid development
-        print!("{esc}[2J{esc}[1;1H {d}", esc = 27 as char, d = rb_msg);
-        print!("Checksum failures {}", checksum_failures);
-        io::stdout().flush().expect("Couldn't flush stdout");
 
         if logger.write(&rb_msg.to_json()).is_err() {
             continue; // do nothing for now
         }
 
-        // Send an update message to our app
-        sender.input(Msg::Update(rb_msg));
+        *telemetry.lock().unwrap() = rb_msg;
+        ctx.request_repaint();
     }
-
     // XXX we don't have a decent way to shut down!
 }
 
 pub fn start() {
-    let app = RelmApp::new("org.openlaps.dashboard");
-    let telemetry = rbmini::message::RbMessage::new();
-    app.run::<DashboardApp>(telemetry);
+    // Log to stdout (if you run with `RUST_LOG=debug`).
+    tracing_subscriber::fmt::init();
+
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(800.0, 600.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Openlaps Dashboard",
+        options,
+        Box::new(|ctx| Box::new(DashboardApp::new(ctx))),
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,7 @@ mod dashboard;
 mod http;
 mod timer;
 
-#[tokio::main]
-async fn main() {
-    // Start the HTTP server
-    tokio::spawn(async move {
-        http::start().await;
-    });
-
+fn main() {
     // Start the GUI
     dashboard::start();
 }

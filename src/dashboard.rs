5f09bad7254bddf2d278fd40c3d2f5b0f6129145
Kris Foster <kris.foster@gmail.com>
2022-12-29 17:10:47 -0800

    Set a minimum frame rate, and pretty print the time

--- src/dashboard.rs
+++ src/dashboard.rs
@@ -4,6 +4,8 @@ use eframe::{egui, CreationContext};
 use std::path::Path;
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::time::Duration;
+use std::{thread, time};
 use tokio::runtime;
 use tokio::sync::mpsc;
 
@@ -15,11 +17,11 @@ use timer::{Lap, LapType, Session};
 use super::http;
 
 const LOG_FILE: &str = "/tmp/openlaps_dashboard_testing.db";
+const FRAME_RATE: u64 = 20; // Desired minimum FPS
 
 macro_rules! send {
     ($ctx:ident, $model:ident, $item:ident, $value:expr) => {
         *$model.$item.lock().unwrap() = $value;
-        $ctx.request_repaint();
     };
 }
 
@@ -69,6 +71,15 @@ impl DashboardApp {
         visuals.override_text_color = Some(egui::Color32::WHITE);
         ctx.egui_ctx.set_visuals(visuals);
 
+        // Set a minimum frame rate
+        let ctx_clone = ctx.egui_ctx.clone();
+        _rt.spawn(async move {
+            loop {
+                ctx_clone.request_repaint();
+                thread::sleep(time::Duration::from_millis(FRAME_RATE / 60));
+            }
+        });
+
         // Start the telemetry updater
         let ctx_clone = ctx.egui_ctx.clone();
         let model_clone = model.clone();
@@ -101,7 +112,7 @@ impl eframe::App for DashboardApp {
                 );
             });
 
-            ui.label(egui::RichText::new(format!("{:.1?}", lap.time())).size(200.0));
+            ui.label(egui::RichText::new(pretty_duration(lap.time())).size(200.0));
 
             ui.label(format!("GPS Coordinates: {}", t.gps_coordinates()));
             ui.label(format!("GPS Fix: {}", t.is_valid_fix()));
@@ -155,6 +166,13 @@ async fn updater(ctx: eframe::egui::Context, model: DashboardModel) {
         send!(ctx, model, telemetry, rb_msg);
     }
     // XXX we don't have a decent way to shut down!
+}
+
+fn pretty_duration(duration: Duration) -> String {
+    let tenths = duration.subsec_millis() / 100;
+    let sec = duration.as_secs() % 60;
+    let min = (duration.as_secs() / 60) % 60;
+    format!("{:0>2}:{:0>2}.{:0>1}", min, sec, tenths)
 }
 
 pub fn start() {
